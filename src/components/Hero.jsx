1f46f3c3cbd8144fefa20215e5126eb9562ef449
CodySawa <kota418@gmail.com>
2023-04-25 05:36:10 -0700

    add github repo link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -8,7 +8,7 @@ const Hero = () => {
 
             <button
                 type='button'
-                onClick={() => window.open('')}
+                onClick={() => window.open('https://github.com/CodySawa/AI-summarizer')}
                 className='black_btn'
             >
                 GitHub
